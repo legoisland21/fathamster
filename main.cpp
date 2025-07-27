5a17ff14931867055567070ae5ec579f318c68fd
legoisland21 <wrzochalmikolaj@gmail.com>
2025-07-27 19:40:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -33,19 +33,19 @@ float Velocity = 0;
 
 string LowerCase(string& str) {
     transform(str.begin(), str.end(), str.begin(),
-        [](unsigned char c) { return tolower(c); });
+        [](unsigned char c) { return tolower(c); }); // Transform to lowercase
     return str;
 }
 
 void HandleCheats() {
-    CodeUi = tinyfd_inputBox("Cheat code", "Enter your desired Cheat code:", "");
-    if (CodeUi == NULL || CodeUi[0] == '\0') return; 
+    CodeUi = tinyfd_inputBox("Cheatcode", "Enter your desired Cheat code:", ""); // Open dialog
+    if (CodeUi == NULL || CodeUi[0] == '\0') return; // Check if the dialog is NULL/empty
     string BasicCode(CodeUi);
-    string Code = LowerCase(BasicCode);
+    string Code = LowerCase(BasicCode); // Make the code lowercase
     if(Code == "infjump") InfJump = !InfJump;
     else if (Code == "unlockfps") { UnlockFps = !UnlockFps; if (UnlockFps) SetTargetFPS(0.5); else SetTargetFPS(60); }
     else if (Code == "showfps") ShowFps = !ShowFps;
-    else if (Code == "changefps") { FPSUi = tinyfd_inputBox("FPS cheat code", "How much FPS?:", "");  if(FPSUi == NULL || FPSUi[0] == '\0') return; LIMITFPS = stoi(FPSUi); SetTargetFPS(LIMITFPS);  }
+    else if (Code == "changefps") { FPSUi = tinyfd_inputBox("FPS cheat code", "How much FPS?:", ""); if(FPSUi == NULL || FPSUi[0] == '\0') return; LIMITFPS = stoi(FPSUi); SetTargetFPS(LIMITFPS);  }
     else if (Code == "moon") { MoonJump = !MoonJump; MoonUi = tinyfd_inputBox("Moon cheat code", "What gravity? (default 0.5f):", ""); if(MoonUi == NULL || MoonUi[0] == '\0') { Gravity = 0.5f; return; } else Gravity = stof(MoonUi); }
 }
 
@@ -55,28 +55,27 @@ int main() {
     Texture2D Ground = LoadTexture("textures/ground.jpg");
     Texture2D Player = LoadTexture("textures/player.png");
     Texture2D PlayerJump = LoadTexture("textures/player_jump.png");
-    if(Sky.id == 0 || Ground.id == 0 || Player.id == 0 || PlayerJump.id == 0) { tinyfd_messageBox("ERROR", "Error: Textures not loaded, game will quit now." , "ok", "error", 1); return -1; }
+    if(Sky.id == 0 || Ground.id == 0 || Player.id == 0 || PlayerJump.id == 0) { tinyfd_messageBox("ERROR", "Error: Textures not loaded, game will quit now." , "ok", "error", 1); return -1; } // Texture check
     InitAudioDevice();
     Sound CheatOn = LoadSound("audio/cheat_on.mp3");
     Sound CheatOff = LoadSound("audio/cheat_off.mp3");
     Music BGM = LoadMusicStream("audio/bgm.mp3");
-    if(CheatOn.frameCount == 0 || CheatOff.frameCount == 0 ) { tinyfd_messageBox("ERROR", "Error: Sounds not loaded, game will quit now." , "ok", "error", 1); return -1; }
-    if(BGM.ctxData == NULL) { tinyfd_messageBox("ERROR", "Error: Music not loaded, game will quit now." , "ok", "error", 1); return -1; }
+    if(CheatOn.frameCount == 0 || CheatOff.frameCount == 0 ) { tinyfd_messageBox("ERROR", "Error: Sounds not loaded, game will quit now." , "ok", "error", 1); return -1; } // SFX Check
+    if(BGM.ctxData == NULL) { tinyfd_messageBox("ERROR", "Error: Music not loaded, game will quit now." , "ok", "error", 1); return -1; } // BGM check
     PlayMusicStream(BGM);
     SetMusicVolume(BGM, 0.15f);
 
-    Vector2 PlayerPos = {0, ScreenHeight - GroundLevel};
+    Vector2 PlayerPos = {0, ScreenHeight - GroundLevel}; // Set player position
 
     SetTargetFPS(60);
     while(!WindowShouldClose()) {
         UpdateMusicStream(BGM);
-        FPS = GetFPS();
 
         if (IsKeyPressed(KEY_SPACE)) {
 			if(!Jumping) {
                 Velocity = JumpForce;
                 if (!InfJump) Jumping = true;
-                else { InfJumping = true; if(Jumping) Jumping = false;}
+                else InfJumping = true;
             }
 		}
 
@@ -95,9 +94,7 @@ int main() {
 
         if(IsKeyPressed(KEY_F9)) { DevMode = !DevMode; if (DevMode) PlaySound(CheatOn); else PlaySound(CheatOff);}
 
-        if(IsKeyPressed(KEY_APOSTROPHE) && DevMode) {
-            HandleCheats();
-        }
+        if(IsKeyPressed(KEY_APOSTROPHE) && DevMode)  HandleCheats();
 
         BeginDrawing();
 
@@ -109,25 +106,18 @@ int main() {
 
         for (int i = 0; i != 7; i++) { DrawTextureEx(Ground, {0 + (i * 250.0f), ScreenHeight - GroundLevel + 100}, 0.0f, GroundScale, WHITE); }
 
-        if (!Jumping && !InfJumping) {
-            DrawTexturePro(Player, SourceRect, DestRect, {0,0}, 0.0f, WHITE);
-        }
-        else if (Jumping || InfJumping) {
-            DrawTexturePro(PlayerJump, SourceRect, DestRect, {0,0}, 0.0f, WHITE);
-        }
-        if (FPS > MFPS) MFPS = FPS;
+        if (!Jumping && !InfJumping) { DrawTexturePro(Player, SourceRect, DestRect, {0,0}, 0.0f, WHITE); } // Draw normal animation
+        else if (Jumping || InfJumping) { DrawTexturePro(PlayerJump, SourceRect, DestRect, {0,0}, 0.0f, WHITE); } // Draw jumping animation
+        FPS = GetFPS();
+        if (FPS > MFPS) MFPS = FPS; // Get max FPS
         if(ShowFps) {
-            x = PlayerPos.x;
-            MouseX = GetMouseX();
-            y = PlayerPos.y;
-            MouseY = GetMouseY();
-            if(y >= 0) y *= 2;
+            Vector2 MousePos = {(float)GetMouseX(), (float)GetMouseY()};
             DrawText(TextFormat("FPS: %d", FPS), 0, 0, 30, BLACK);
             DrawText(TextFormat("Max FPS: %d", MFPS), 250, 0, 30, BLACK);
-            DrawText(TextFormat("X Position: %d", x), 0, 30, 30, BLACK);
-            DrawText(TextFormat("Y Position: %d", y), 250, 30, 30, BLACK);
-            DrawText(TextFormat("Mouse X: %d", MouseX), 0, 60, 30, BLACK);
-            DrawText(TextFormat("Mouse Y: %d", MouseY), 250, 60, 30, BLACK);
+            DrawText(TextFormat("X Position: %d", (int)PlayerPos.x), 0, 30, 30, BLACK);
+            DrawText(TextFormat("Y Position: %d", (int)PlayerPos.y), 250, 30, 30, BLACK);
+            DrawText(TextFormat("Mouse X: %d", (int)MousePos.x), 0, 60, 30, BLACK);
+            DrawText(TextFormat("Mouse Y: %d", (int)MousePos.y), 250, 60, 30, BLACK);
             DrawText(TextFormat("Gravity: %.4g", Gravity), 0, 90, 30, BLACK);
             DrawText(TextFormat("Velocity (Y): %.4g", Velocity), 250, 90, 30, BLACK);
             if (IsKeyDown(KEY_SPACE)) DrawRectangle(1212, 110, 100, 20, WHITE);
